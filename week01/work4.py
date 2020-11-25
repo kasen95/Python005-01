b42b87ee9eec562c9f354625d24d72576c873eb5
kasen95 <15627868195@163.com>
2020-11-25 21:23:31 +0800

    fix：增加注释说明

--- week01/work4.py
+++ week01/work4.py
@@ -8,7 +8,7 @@ def test():
     # log_path = os.path.join('/Users/shaojiacheng/python-' + time.strftime('%Y-%m-%d', time.localtime(time.time())),'test.log')
     log_path = os.path.join('/var/log/python-' + time.strftime('%Y-%m-%d', time.localtime(time.time())), 'test.log')
     parent_path = os.path.abspath(os.path.join(log_path, '..')) + '/'
-    # 需提前获取该目录下写入权限，这种方式还是获取不了
+    # 需先获取该目录下写入权限，这种方式还是获取不了。使用注释的目录正常
     if not os.access('/var/log/', os.W_OK):
         os.chmod('/var/log/', 0o755)
     if not os.path.isdir(parent_path):
